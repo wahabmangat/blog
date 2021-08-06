1d76f84e1442236e5eec0acc3dc8c0df118ddafc
wahabmangat <abdulwahabmangat@gmail.com>
2021-08-06 15:22:07 +0500

    Choose layout at runtime.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
 class ArticlesController < ApplicationController
     http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+    layout :article_layout
     def index
       @articles = Article.all
   end
-  
   def show
     @article = Article.find(params[:id])
   end
   def new
      @article = Article.new
+     #render body: "raw"
   end
   def create
      @article = Article.new(article_params)
@@ -35,9 +36,13 @@ class ArticlesController < ApplicationController
       @article.destroy
 
       redirect_to root_path
+       
    end
   private
   def article_params
       params.require(:article).permit(:title, :body, :status)
+  end
+  def article_layout
+    "application"
   end
 end
